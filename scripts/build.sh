0663651e1dc479bcd749a7e90e012ea320e1de02
pacobarrachina <pacobarrachina.mari@gmail.com>
2019-10-02 18:08:02 +0200

    Envio página marvelo

---
+++ scripts/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf C:/Users/Francisco/Documents/DAW2/DespliegueAPP/MARVELO/dist
+
+mkdir C:/Users/Francisco/Documents/DAW2/DespliegueAPP/MARVELO/dist
+
+cp -r C:/Users/Francisco/Documents/DAW2/DespliegueAPP/MARVELO/src/index.html C:/Users/Francisco/Documents/DAW2/DespliegueAPP/MARVELO/dist
---
+++ scripts/deploy.sh
@@ -0,0 +1,3 @@
+rm -rf C:/xampp/htdocs/despliegueAPP/MARVELO
+
+cp -r C:/Users/Francisco/Documents/DAW2/DespliegueAPP/MARVELO C:/xampp/htdocs/despliegueAPP
